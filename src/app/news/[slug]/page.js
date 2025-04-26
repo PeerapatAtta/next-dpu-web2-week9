a218f3ffa23b9dbf9948e3f5ac942d8319bfa5bf
OFF <peerapat13th@gmail.com>
2025-04-26 19:17:42 +0700

    Week 10 done

--- src/app/news/[slug]/page.js
+++ src/app/news/[slug]/page.js
@@ -1,19 +1,22 @@
 import { DUMMY_NEWS } from "@/data/dummy-news";
 import { notFound } from "next/navigation";
+import Link from 'next/link';
 
 export default function NewsContentPage({ params }) {
     const slug = params.slug;
     const newsItem = DUMMY_NEWS.find((news) => news.slug === slug);
-    
+
     if (!newsItem) notFound(); // If the news item is not found, show a 404 page
-    
+
     return (
         <article className="news-article">
             <header>
-                <img
-                    src={`/images/news/${newsItem.image}`}
-                    alt={newsItem.title}
-                />
+                <Link href={`/news/${slug}/image`}>
+                    <img
+                        src={`/images/news/${newsItem.image}`}
+                        alt={newsItem.title}
+                    />
+                </Link>
                 <h1>{newsItem.title}</h1>
                 <time dateTime={newsItem.date}>{newsItem.date}</time>
             </header>
